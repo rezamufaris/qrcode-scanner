baf6cd9fb20aec7eae726f3de99faf26a7ecf914
Muhammad Fahreza Mufaris <202110715079@mhs.ubharajaya.ac.id>
2022-08-10 21:43:16 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# QR Code Scanner
+
+Project ini merupakan Project membuat QR Code Scanner Sederhana menggunakan HTML, CSS dan JavaScript. QR Code Generator ini dibuat dengan referensi video youtube [CodingNepal](https://www.youtube.com/c/CodingNepal).
+
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://rezamufaris.github.io/portfolio)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/fahrezamufaris)
+[![Instagram](https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/rezamufaris)
+[![YouTube](https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white)](https://www.youtube.com/c/MufarisFahrezaChannel)
+[![Tiktok](https://img.shields.io/badge/TikTok-000000?style=for-the-badge&logo=tiktok&logoColor=white)](https://www.tiktok.com/@mufarisfahreza)
+
+
+
+## Demo
+
+Try my project by visit this url `https://rezamufaris.github.io/qrcode-scanner` or click this [Fahreza's QR Code Scanner](https://rezamufaris.github.io/qrcode-scanner).
+
---
+++ script.js
@@ -0,0 +1,43 @@
+const wrapper = document.querySelector('.wrapper'),
+  form = document.querySelector('form'),
+  fileInp = form.querySelector('input'),
+  infoText = form.querySelector('p'),
+  closeBtn = document.querySelector('.close'),
+  copyBtn = document.querySelector('.copy');
+
+function fetchRequest(formData, file) {
+  infoText.innerText = 'Scanning QR Code...';
+  // Sending post request to qr server api with passing form data as body and getting response from it
+  fetch('https://api.qrserver.com/v1/read-qr-code/', {
+    method: 'POST',
+    body: formData,
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      result = result[0].symbol[0].data;
+      infoText.innerText = result ? 'Upload QR Code to Scan' : 'Can Not Scan QR Code';
+      if (!result) return;
+      wrapper.querySelector('textarea').innerText = result;
+      form.querySelector('img').src = URL.createObjectURL(file);
+      wrapper.classList.add('active');
+    })
+    .catch(() => {
+      infoText.innerText = 'Can Not Scan QR Code';
+    });
+}
+
+fileInp.addEventListener('change', async (e) => {
+  let file = e.target.files[0]; // Getting user selected file
+  if (!file) return;
+  let formData = new FormData(); // Creating a new FormData object
+  formData.append('file', file); // Adding selected file to formData
+  fetchRequest(formData, file);
+});
+
+copyBtn.addEventListener('click', () => {
+  let text = document.querySelector('textarea').textContent;
+  navigator.clipboard.writeText(text);
+});
+
+form.addEventListener('click', () => fileInp.click());
+closeBtn.addEventListener('click', () => wrapper.classList.remove('active'));
